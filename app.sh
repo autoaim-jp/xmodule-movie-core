2c8e6463b539569f7fe389674fda5595d96ef2d4
autoaim_jp <git@autoaim.jp>
2024-10-20 18:08:48 +0900

    feat: title fadein fadeout part movie

--- app.sh
+++ app.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-DATA_DIR=./data/tmp/title_image/
-TITLE_IMG_FILE_NAME=$(date '+%Y%m%d_%H%M%S').png
+DATA_PATH="${PWD}/data/"
+CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
+TITLE_IMG_FILE_PATH=${DATA_PATH}tmp/title_image/${CURRENT_TIME}.png
+TITLE_MOVIE_FILE_PATH=${DATA_PATH}tmp/title_movie/${CURRENT_TIME}.mp4
 
 TITLE_TEXT=${1:-"Sampleアプリ\n操作方法の紹介"}
 
 # タイトル画像作成
-./core/createTitleImage.sh ${DATA_DIR}${TITLE_IMG_FILE_NAME} ${TITLE_TEXT}
+./core/createTitleImage.sh $TITLE_IMG_FILE_PATH $TITLE_TEXT
+echo "作成したタイトル画像: $TITLE_IMG_FILE_PATH"
+# xdg-open $TITLE_IMG_FILE_PATH
 
+# フェードインとフェードアウトの動画
+./core/fadeEffect.sh $TITLE_IMG_FILE_PATH $TITLE_MOVIE_FILE_PATH
+echo "作成したタイトル動画: $TITLE_MOVIE_FILE_PATH"
+# xdg-open $TITLE_MOVIE_FILE_PATH
 
 # tmpディレクトリのクリーンアップ
 rm ./data/tmp/tmp/*
---
+++ core/fadeEffect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TMP_FADEIN_FILE_PATH="${PWD}/data/tmp/tmp/__fadein.mp4"
+TITLE_IMG_FILE_PATH=${1:-"${PWD}/data/src/sample/title.png"}
+TITLE_MOVIE_FILE_PATH=${2:-/tmp/title.mp4}
+
+ffmpeg -loop 1 -i $TITLE_IMG_FILE_PATH -vf "fade=in:0:30" -t 5 $TMP_FADEIN_FILE_PATH
+
+ffmpeg -i $TMP_FADEIN_FILE_PATH -vf "fade=out:120:30" $TITLE_MOVIE_FILE_PATH
