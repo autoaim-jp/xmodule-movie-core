f0c25565f8a4d72f8d770309806ba9e368f613fa
autoaim_jp <git@autoaim.jp>
2024-10-22 07:00:43 +0900

    fix: use sox to concat instead of ffmpeg

--- core/concatWav.sh
+++ core/concatWav.sh
@@ -4,7 +4,9 @@ WAV_LIST_FILE_PATH=${1:-"${PWD}/data/src/project/sample/wav_list_for_ffmpeg.txt"
 CONCAT_SOUND_FILE_PATH=${2:-/tmp/concat.wav}
 
 # テキストファイル内のwavファイルを順に結合
-ffmpeg -f concat -safe 0 -i $WAV_LIST_FILE_PATH -c copy $CONCAT_SOUND_FILE_PATH > /dev/null 2>&1
+# なぜか早口になってしまう
+# ffmpeg -f concat -safe 0 -i $WAV_LIST_FILE_PATH -c copy $CONCAT_SOUND_FILE_PATH > /dev/null 2>&1
+sox $(<$WAV_LIST_FILE_PATH) $CONCAT_SOUND_FILE_PATH
 
 # 結合したwavファイルの長さを表示
 echo "結合したwavファイルの長さ: $(soxi -d $CONCAT_SOUND_FILE_PATH) または $(soxi -D $CONCAT_SOUND_FILE_PATH) 秒"
