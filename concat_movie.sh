0c23221c5cfb24f0431ebf32e13197c49c574acb
autoaim_jp <git@autoaim.jp>
2024-11-18 16:10:48 +0900

    refactoring: concat_movie.sh

--- concat_movie.sh
+++ concat_movie.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR_PATH=$(dirname "$0")/
-SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}../data/src/project/sample/
-TMP_DIR_PATH=${SCRIPT_DIR_PATH}../data/tmp/tmp/
+SCRIPT_DIR_PATH=${PWD}/
+DATA_DIR_PATH="${SCRIPT_DIR_PATH}/data/"
+SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}data/src/project/sample/
+CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
 
 # output
 OUTPUT_DIR_PATH=${DATA_DIR_PATH}generated/concat_movie/ 
@@ -10,18 +11,18 @@ mkdir -p $OUTPUT_DIR_PATH
 FINAL_MOVIE_FILE_PATH=${OUTPUT_DIR_PATH}${CURRENT_TIME}.mp4
 
 # input
-TITLE_MOVIE_FILE_PATH=${1:-"${DATA_DIR_PATH}/src/project/sample/title_movie.mp4"}
-SUBTITLE_MOVIE_FILE_PATH=${2:-"${DATA_DIR_PATH}/src/project/sample/capture_subtitle.mp4"}
-CONCAT_SOUND_FILE_PATH=${3:-"${DATA_DIR_PATH}/src/project/sample/concat_sound.wav"}
+TITLE_MOVIE_FILE_PATH=${1:-"${SAMPLE_PROJECT_DIR_PATH}title_movie.mp4"}
+SUBTITLE_MOVIE_FILE_PATH=${2:-"${SAMPLE_PROJECT_DIR_PATH}capture_subtitle.mp4"}
+CONCAT_SOUND_FILE_PATH=${3:-"${SAMPLE_PROJECT_DIR_PATH}concat_sound.wav"}
 
 # tmp
 TMP_DIR_PATH=${DATA_DIR_PATH}tmp/concat_movie/ 
 mkdir -p $TMP_DIR_PATH
 CONCAT_MOVIE_FILE_PATH=${TMP_DIR_PATH}${CURRENT_TIME}.mp4
 
-./core/concatMovie.sh $TITLE_MOVIE_FILE_PATH $SUBTITLE_MOVIE_FILE_PATH $CONCAT_MOVIE_FILE_PATH
+./core/concatMovie.sh $CONCAT_MOVIE_FILE_PATH $TITLE_MOVIE_FILE_PATH $SUBTITLE_MOVIE_FILE_PATH
 echo "タイトルとキャプチャの結合動画: $CONCAT_MOVIE_FILE_PATH"
 
-./core/concatWavAndMovie.sh $CONCAT_SOUND_FILE_PATH $CONCAT_MOVIE_FILE_PATH $FINAL_MOVIE_FILE_PATH
+./core/concatWavAndMovie.sh $FINAL_MOVIE_FILE_PATH $CONCAT_SOUND_FILE_PATH $CONCAT_MOVIE_FILE_PATH
 echo "動画と音声の結合結果: $FINAL_MOVIE_FILE_PATH"
 
