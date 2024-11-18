01116744c2372ad78e546318d9aca2e93cea44c1
autoaim_jp <git@autoaim.jp>
2024-11-18 15:48:17 +0900

    feat: mkdir output, tmp dir

--- title_movie.sh
+++ title_movie.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-DATA_DIR_PATH="${PWD}/data/"
+SCRIPT_DIR_PATH=$(dirname "$0")/
+DATA_DIR_PATH="${SCRIPT_DIR_PATH}/data/"
+##SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}../data/src/project/sample/
 CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
 
 # output
-TITLE_MOVIE_FILE_PATH=${DATA_DIR_PATH}generated/title_movie/${CURRENT_TIME}.mp4
-mkdir -p ${DATA_DIR_PATH}generated/title_movie/
+OUTPUT_DIR_PATH=${DATA_DIR_PATH}generated/title_movie/
+mkdir -p $OUTPUT_DIR_PATH
+TITLE_MOVIE_FILE_PATH=${OUTPUT_DIR_PATH}${CURRENT_TIME}.mp4
 
 # input
 TITLE_TEXT=${1:-"Sampleアプリ\n操作方法の紹介"}
 
 # tmp 
-TITLE_IMG_FILE_PATH=${DATA_DIR_PATH}tmp/title_movie/${CURRENT_TIME}.png
-mkdir -p ${DATA_DIR_PATH}tmp/title_movie/
+TMP_DIR_PATH=${DATA_DIR_PATH}tmp/title_movie/
+mkdir -p $TMP_DIR_PATH
+TITLE_IMG_FILE_PATH=${TMP_DIR_PATH}${CURRENT_TIME}.png
 
 # タイトル画像作成
 ./core/createTitleImage.sh $TITLE_IMG_FILE_PATH $TITLE_TEXT
 echo "作成したタイトル画像: $TITLE_IMG_FILE_PATH"
-# xdg-open $TITLE_IMG_FILE_PATH
 
 # フェードインとフェードアウトの動画
 ./core/fadeEffect.sh $TITLE_MOVIE_FILE_PATH $TITLE_IMG_FILE_PATH
 echo "作成したタイトル動画: $TITLE_MOVIE_FILE_PATH"
-# xdg-open $TITLE_MOVIE_FILE_PATH
 
