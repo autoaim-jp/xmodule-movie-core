5dc0be2dcfeda212f3a41719c2fb0783e8f36728
autoaim_jp <git@autoaim.jp>
2024-11-18 16:52:49 +0900

    refactoring: all files

--- core/createBasePartMovie.sh
+++ core/createBasePartMovie.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-BASE_MOVIE_PATH=${1:-"${PWD}/data/src/project/sample/main_part/base.mp4"}  # ベース動画
+SCRIPT_DIR_PATH=$(dirname "$0")/
+SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}../data/src/project/sample/
+##TMP_DIR_PATH=${SCRIPT_DIR_PATH}../data/tmp/tmp/
 
-CENTER_IMAGE_LIST_FILE_PATH=${2:-"${PWD}/data/src/project/sample/image_list_number.txt"}  # 中央に標示する画像のリスト
-RT_OWNER_IMAGE_FILE_PATH=${3:-"${PWD}/data/src/project/sample/main_part/right_top.png"}
-LT_THEME_IMAGE_FILE_PATH=${4:-"${PWD}/data/src/project/sample/main_part/left_top.png"}
-RB_TOPIC_IMAGE_FILE_PATH=${5:-"${PWD}/data/src/project/sample/main_part/right_bottom.png"}
-LB_NARRATOR_IMAGE_FILE_PATH=${6:-"${PWD}/data/src/project/sample/asset/woman_flop.png"}    # 左下に表示する画像
-BACKGROUND_IMAGE_FILE_PATH=${7:-"${PWD}/data/src/project/sample/asset/background.jpg"}    # 全画面の背景画像
+# output
+BASE_MOVIE_PATH=${1:-"${SAMPLE_PROJECT_DIR_PATH}main_part/base.mp4"}  # ベース動画
 
+# input
+CENTER_IMAGE_LIST_FILE_PATH=${2:-"${SAMPLE_PROJECT_DIR_PATH}image_list_number.txt"}  # 中央に標示する画像のリスト
+RT_OWNER_IMAGE_FILE_PATH=${3:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_top.png"}
+LT_THEME_IMAGE_FILE_PATH=${4:-"${SAMPLE_PROJECT_DIR_PATH}main_part/left_top.png"}
+RB_TOPIC_IMAGE_FILE_PATH=${5:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_bottom.png"}
+LB_NARRATOR_IMAGE_FILE_PATH=${6:-"${SAMPLE_PROJECT_DIR_PATH}asset/woman_flop.png"}    # 左下に表示する画像
+BACKGROUND_IMAGE_FILE_PATH=${7:-"${SAMPLE_PROJECT_DIR_PATH}asset/background.jpg"}    # 全画面の背景画像
+
+# other
 # 画像サイズと位置
 output_width=1920                     # 出力動画の幅
 output_height=1080                    # 出力動画の高さ
@@ -24,14 +31,14 @@ left_bottom_image_height=151          # 左下画像の高さ
 total_part_sec=0
 
 # total_part_secの最終の値を確認
-while IFS=',' read -r partSec fadeInSec fadeOutSec filePath
+while IFS=',' read -r part_sec fade_in_sec fade_out_sec filePath
 do
     # ヘッダ行（#で始まる行）や空行はスキップ
-    if [[ "$partSec" =~ ^#.* ]] || [[ -z "$partSec" ]]; then
+    if [[ "$part_sec" =~ ^#.* ]] || [[ -z "$part_sec" ]]; then
         continue
     fi
     # total_part_secの更新
-    total_part_sec=$((total_part_sec + partSec))
+    total_part_sec=$((total_part_sec + part_sec))
 done < "$CENTER_IMAGE_LIST_FILE_PATH"
 
 echo $total_part_sec
@@ -49,5 +56,4 @@ ffmpeg -y -i "$BACKGROUND_IMAGE_FILE_PATH" -i "$LB_NARRATOR_IMAGE_FILE_PATH" -i 
     [bg_with_right_top_image][bottom_left_img]overlay=x=20:y=H-${left_bottom_image_height}-20[bg_with_left_bottom_image]; \
     [bg_with_left_bottom_image][scaled_right_bottom_image]overlay=x=W-w-10:y=H-h-10" \
 -c:v libx264 -t $total_part_sec -pix_fmt yuv420p $BASE_MOVIE_PATH
-
 
--- core/voicepeak.sh
+++ core/voicepeak.sh
@@ -6,8 +6,8 @@ SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}../data/src/project/sample/
 
 # output
 WAV_LIST_FILE_PATH=${1:-/tmp/wav_list_for_ffmpeg.txt}
-SOUND_DIR=${2:-/tmp/sound/}
-mkdir -p $SOUND_DIR
+SOUND_DIR_PATH=${2:-/tmp/sound/}
+mkdir -p $SOUND_DIR_PATH
 
 # input
 NARRATION_FILE_PATH=${3:-"${SAMPLE_PROJECT_DIR_PATH}narration.csv"}
@@ -29,12 +29,12 @@ declare -A NARRATOR_LIST=(
 # 音声ファイルを作成
 function _speak() {
   sleep 1
-  OUTPUT_FILE_PATH=$1
+  output_file_path=$1
   _NARRATOR=$2
   SPEED=$3
   EMOTION_EXPR=$4
   TEXT=$5
-  echo $OUTPUT_FILE_PATH
+  echo $output_file_path
   if [[ -n "${NARRATOR_LIST[$_NARRATOR]}" ]]; then
       NARRATOR=${NARRATOR_LIST[$_NARRATOR]}
   else
@@ -42,15 +42,15 @@ function _speak() {
 	  exit 1
   fi
 
-  ~/application/Voicepeak/voicepeak -o $OUTPUT_FILE_PATH -n "$NARRATOR" --speed $SPEED -e "$EMOTION_EXPR" -s "$TEXT" 2>/dev/null
+  ~/application/Voicepeak/voicepeak -o $output_file_path -n "$NARRATOR" --speed $SPEED -e "$EMOTION_EXPR" -s "$TEXT" 2>/dev/null
   if [[ $? -eq 0 ]]; then
     echo "音声ファイルの作成に成功。"
     return 0
   else
     echo "音声ファイルの作成に失敗しました。再度生成します。"
-    echo "$OUTPUT_FILE_PATH,$_NARRATOR,$NARRATOR,$SPEED,$EMOTION_EXPR,$TEXT"
+    echo "$output_file_path,$_NARRATOR,$NARRATOR,$SPEED,$EMOTION_EXPR,$TEXT"
     sleep 3
-    ~/application/Voicepeak/voicepeak -o $OUTPUT_FILE_PATH -n "$NARRATOR" --speed $SPEED -e "$EMOTION_EXPR" -s "$TEXT" 2>/dev/null
+    ~/application/Voicepeak/voicepeak -o $output_file_path -n "$NARRATOR" --speed $SPEED -e "$EMOTION_EXPR" -s "$TEXT" 2>/dev/null
     if [[ $? -ne 0 ]]; then
       echo "音声ファイルの作成に失敗しました。終了します。"
       return 1
@@ -69,33 +69,33 @@ cat $NARRATION_FILE_PATH | while IFS=',' read -r type arg1 arg2 arg3 arg4; do
   # 行数をインクリメント
   ((LINE_NUMBER++))
   # 音声ファイル名 すでに存在していれば利用 なければ作成 ファイル名を安全にするためシェルの特殊文字とスラッシュをアンダースコアで置換
-  OUTPUT_FILE_NAME=$(printf "%s_%s_%s_%s_%s.wav" "$type" "$arg1" "$arg2" "$arg3" "$arg4" | sed 's/[*?[\]{}()&|;<>`"'"'"'\\$#\/]/_/g')
-  OUTPUT_FILE_PATH="${SOUND_DIR}${OUTPUT_FILE_NAME}"
+  output_file_name=$(printf "%s_%s_%s_%s_%s.wav" "$type" "$arg1" "$arg2" "$arg3" "$arg4" | sed 's/[*?[\]{}()&|;<>`"'"'"'\\$#\/]/_/g')
+  output_file_path="${SOUND_DIR_PATH}${output_file_name}"
 
   # 音声ファイルがなければ作成
-  if [[ ! -f $OUTPUT_FILE_PATH ]]; then
+  if [[ ! -f $output_file_path ]]; then
     if [[ $type == "speak" ]]; then
-      _speak $OUTPUT_FILE_PATH $arg1 $arg2 $arg3 $arg4
+      _speak $output_file_path $arg1 $arg2 $arg3 $arg4
     elif [[ $type == "silent" ]]; then
-      SILENT_TIME_S=$arg1
-      sox -n -r 48000 -c 1 $OUTPUT_FILE_PATH trim 0 $SILENT_TIME_S >/dev/null 2>&1
+      silent_time_s=$arg1
+      sox -n -r 48000 -c 1 $output_file_path trim 0 $silent_time_s >/dev/null 2>&1
     else
       echo "未対応のtype: $type $arg1 $arg2 $arg3 $arg4"
       exit 1
     fi
   
     # 音声ファイルが生成できなければ異常終了
-    if [[ ! -f $OUTPUT_FILE_PATH ]]; then
+    if [[ ! -f $output_file_path ]]; then
       exit 1
     fi
   fi
 
   # 音声ファイルをwavリストに追記
-  echo "$OUTPUT_FILE_PATH" >> $WAV_LIST_FILE_PATH
+  echo "$output_file_path" >> $WAV_LIST_FILE_PATH
   # ffmpeg用
-  # echo "file '$OUTPUT_FILE_PATH'" >> $WAV_LIST_FILE_PATH
+  # echo "file '$output_file_path'" >> $WAV_LIST_FILE_PATH
 
   # 音声ファイルの長さを表示
-  echo $(soxi -D $OUTPUT_FILE_PATH) $OUTPUT_FILE_PATH
+  echo $(soxi -D $output_file_path) $output_file_path
 done
 
