d3deb6ca489c52c4159b25fb82f9349d698d822d
autoaim_jp <git@autoaim.jp>
2024-11-18 19:59:28 +0900

    feat: add title_movie.mp4

--- title_movie.sh
+++ title_movie.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR_PATH=${PWD}/
 DATA_DIR_PATH="${SCRIPT_DIR_PATH}data/"
-##SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}data/src/project/sample/
+SAMPLE_PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}data/src/project/sample/
 CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
 
 # output
@@ -12,6 +12,7 @@ TITLE_MOVIE_FILE_PATH=${OUTPUT_DIR_PATH}${CURRENT_TIME}.mp4
 
 # input
 TITLE_TEXT=${1:-"Sampleアプリ\n操作方法の紹介"}
+LOGO_IMG_FILE_PATH=${2:-"${SAMPLE_PROJECT_DIR_PATH}logo.webp"}
 
 # tmp 
 TMP_DIR_PATH=${DATA_DIR_PATH}tmp/title_movie/
@@ -19,7 +20,7 @@ mkdir -p $TMP_DIR_PATH
 TITLE_IMG_FILE_PATH=${TMP_DIR_PATH}${CURRENT_TIME}.png
 
 # タイトル画像作成
-./core/createTitleImage.sh $TITLE_IMG_FILE_PATH $TITLE_TEXT
+./core/createTitleImage.sh $TITLE_IMG_FILE_PATH $TITLE_TEXT $LOGO_IMG_FILE_PATH
 echo "作成したタイトル画像: $TITLE_IMG_FILE_PATH"
 
 # フェードインとフェードアウトの動画
