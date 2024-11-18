98f2704d91592dfa8dbf644f8456afe9105187ef
autoaim_jp <git@autoaim.jp>
2024-11-18 15:15:20 +0900

    refactoring: title.sh

--- speak.sh
+++ speak.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DATA_PATH="${PWD}/data/"
+DATA_DIR_PATH="${PWD}/data/"
 CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
-WAV_LIST_FILE_PATH=${DATA_PATH}tmp/wav_list/${CURRENT_TIME}_for_ffmpeg.txt
-SOUND_DIR=${DATA_PATH}generated/sound/
-CONCAT_SOUND_FILE_PATH=${DATA_PATH}tmp/sound_concat/${CURRENT_TIME}.wav
+WAV_LIST_FILE_PATH=${DATA_DIR_PATH}tmp/wav_list/${CURRENT_TIME}_for_ffmpeg.txt
+SOUND_DIR=${DATA_DIR_PATH}generated/sound/
+CONCAT_SOUND_FILE_PATH=${DATA_DIR_PATH}tmp/sound_concat/${CURRENT_TIME}.wav
 
-NARRATION_FILE_PATH=${1:-"${DATA_PATH}src/project/sample/narration.csv"}
+NARRATION_FILE_PATH=${1:-"${DATA_DIR_PATH}src/project/sample/narration.csv"}
 
 ./core/voicepeak.sh $NARRATION_FILE_PATH $WAV_LIST_FILE_PATH $SOUND_DIR
 if [[ $? -ne 0 ]]; then
--- subtitle.sh
+++ subtitle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DATA_PATH="${PWD}/data/"
+DATA_DIR_PATH="${PWD}/data/"
 CURRENT_TIME=$(date '+%Y%m%d_%H%M%S')
 CAPTURE_MOVIE_FILE_PATH=${1:-"${PWD}/data/src/project/sample/capture.mp4"}
 SUBTITLE_TEXT_FILE_PATH=${2:-"${PWD}/data/src/project/sample/subtitle.csv"}
-SUBTITLE_MOVIE_FILE_PATH=${DATA_PATH}tmp/capture_subtitle/${CURRENT_TIME}.mp4
+SUBTITLE_MOVIE_FILE_PATH=${DATA_DIR_PATH}tmp/capture_subtitle/${CURRENT_TIME}.mp4
 
 ./core/addSubtitle.sh $CAPTURE_MOVIE_FILE_PATH $SUBTITLE_TEXT_FILE_PATH $SUBTITLE_MOVIE_FILE_PATH
 echo "字幕動画: $SUBTITLE_MOVIE_FILE_PATH"
