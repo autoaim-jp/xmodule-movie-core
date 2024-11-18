3603f2d82ca92b601357ed57cae3e3bf8e23db84
autoaim_jp <git@autoaim.jp>
2024-11-18 20:41:30 +0900

    feat: update some files

--- core/createBasePartMovie.sh
+++ core/createBasePartMovie.sh
@@ -9,20 +9,16 @@ BASE_MOVIE_PATH=${1:-"/tmp/base.mp4"}  # ベース動画
 
 # input
 CENTER_IMAGE_LIST_FILE_PATH=${2:-"${SAMPLE_PROJECT_DIR_PATH}image_list_number.txt"}
-RT_OWNER_IMAGE_FILE_PATH=${3:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_top.png"}
-LT_THEME_IMAGE_FILE_PATH=${4:-"${SAMPLE_PROJECT_DIR_PATH}main_part/left_top.png"}
-RB_TOPIC_IMAGE_FILE_PATH=${5:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_bottom.png"}
-LB_NARRATOR_IMAGE_FILE_PATH=${6:-"${SAMPLE_PROJECT_DIR_PATH}asset/woman_flop.png"}
-BACKGROUND_IMAGE_FILE_PATH=${7:-"${SAMPLE_PROJECT_DIR_PATH}asset/background.jpg"}
+BACKGROUND_IMAGE_FILE_PATH=${3:-"${SAMPLE_PROJECT_DIR_PATH}asset/background.jpg"}
+RT_OWNER_IMAGE_FILE_PATH=${4:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_top.png"}
+LT_THEME_IMAGE_FILE_PATH=${5:-"${SAMPLE_PROJECT_DIR_PATH}main_part/left_top.png"}
+RB_TOPIC_IMAGE_FILE_PATH=${6:-"${SAMPLE_PROJECT_DIR_PATH}main_part/right_bottom.png"}
+LB_NARRATOR_IMAGE_FILE_PATH=${7:-"${SAMPLE_PROJECT_DIR_PATH}asset/woman_flop.png"}
 
 # other
 # 画像サイズと位置
 output_width=1920                     # 出力動画の幅
 output_height=1080                    # 出力動画の高さ
-right_top_box_width=300               # 右上に表示するボックスの幅
-right_top_box_height=100              # 右上ボックスの高さ
-center_image_width=600                # 中央画像の幅
-center_image_height=400               # 中央画像の高さ
 left_bottom_image_width=200           # 左下に表示するナレーター画像の幅
 left_bottom_image_height=151          # 左下画像の高さ
 
@@ -42,17 +38,29 @@ done < "$CENTER_IMAGE_LIST_FILE_PATH"
 
 echo $total_part_sec
 
-# ベース動画作成　BG画像表示。四隅に画像配置
-ffmpeg -y -i "$BACKGROUND_IMAGE_FILE_PATH" -i "$LB_NARRATOR_IMAGE_FILE_PATH" -i "$RT_OWNER_IMAGE_FILE_PATH" -i "$RB_TOPIC_IMAGE_FILE_PATH" -i "$LT_THEME_IMAGE_FILE_PATH" \
--filter_complex "\
-    [0:v]scale=${output_width}:${output_height}[bg]; \
-    [2:v]scale=200:80[scaled_right_top_image]; \
-    [3:v]scale=380:100[scaled_right_bottom_image]; \
-    [4:v]scale=655:100[scaled_left_top_image]; \
-    [bg][scaled_left_top_image]overlay=x=20:y=20[bg_with_left_top_image]; \
-    [bg_with_left_top_image][scaled_right_top_image]overlay=x=W-w-10:y=20[bg_with_right_top_image]; \
-    [1:v]scale=${left_bottom_image_width}:${left_bottom_image_height}[bottom_left_img]; \
-    [bg_with_right_top_image][bottom_left_img]overlay=x=20:y=H-${left_bottom_image_height}-20[bg_with_left_bottom_image]; \
-    [bg_with_left_bottom_image][scaled_right_bottom_image]overlay=x=W-w-10:y=H-h-10" \
--c:v libx264 -t $total_part_sec -pix_fmt yuv420p $BASE_MOVIE_PATH
+# ベース動画作成　BG画像表示
+if [[ "$RT_OWNER_IMAGE_FILE_PATH" == "x" && \
+      "$LT_THEME_IMAGE_FILE_PATH" == "x" && \
+      "$RB_TOPIC_IMAGE_FILE_PATH" == "x" && \
+      "$LB_NARRATOR_IMAGE_FILE_PATH" == "x" ]]; then
+  echo "テロップなし"
+  ffmpeg -y -i "$BACKGROUND_IMAGE_FILE_PATH" \
+  -filter_complex "\
+      [0:v]scale=${output_width}:${output_height}" \
+  -c:v libx264 -t $total_part_sec -pix_fmt yuv420p $BASE_MOVIE_PATH
+else
+  echo "テロップあり"
+  ffmpeg -y -i "$BACKGROUND_IMAGE_FILE_PATH" -i "$LB_NARRATOR_IMAGE_FILE_PATH" -i "$RT_OWNER_IMAGE_FILE_PATH" -i "$RB_TOPIC_IMAGE_FILE_PATH" -i "$LT_THEME_IMAGE_FILE_PATH" \
+  -filter_complex "\
+      [0:v]scale=${output_width}:${output_height}[bg]; \
+      [1:v]scale=${left_bottom_image_width}:${left_bottom_image_height}[bottom_left_img]; \
+      [2:v]scale=200:80[scaled_right_top_image]; \
+      [3:v]scale=380:100[scaled_right_bottom_image]; \
+      [4:v]scale=655:100[scaled_left_top_image]; \
+      [bg][scaled_left_top_image]overlay=x=20:y=20[bg_with_left_top_image]; \
+      [bg_with_left_top_image][scaled_right_top_image]overlay=x=W-w-10:y=20[bg_with_right_top_image]; \
+      [bg_with_right_top_image][bottom_left_img]overlay=x=20:y=H-${left_bottom_image_height}-20[bg_with_left_bottom_image]; \
+      [bg_with_left_bottom_image][scaled_right_bottom_image]overlay=x=W-w-10:y=H-h-10" \
+  -c:v libx264 -t $total_part_sec -pix_fmt yuv420p $BASE_MOVIE_PATH
+fi
 
--- core/mergeBaseAndFadeMovie.sh
+++ core/mergeBaseAndFadeMovie.sh
@@ -17,6 +17,13 @@ BASE_MOVIE_PATH=${4:-"${SAMPLE_PROJECT_DIR_PATH}main_part/base.mp4"}
 # tmp
 FADE_MOVIE_PATH=${TMP_DIR_PATH}__image_fade.mov
 
+# other
+# 1920x1080ならこれがいい
+# center_image_width=1422                # 中央画像の幅
+# center_image_height=800               # 中央画像の高さ
+center_image_width=1024                # 中央画像の幅
+center_image_height=1024              # 中央画像の高さ
+
 # 必要なもの: 動画パート時間(10),フェードイン(4),フェードアウト(4),画像パス→
 # 計算で求められるもの: フェードアウト開始(6 動画パート時間(10)-フェードアウト(4)), オーバーラップさせる時間(t,0,10 これまでオーバーラップされた時間(変数sum_part_sec 動画パート時間の累積)と、それ+動画パート時間(10))
 
@@ -33,7 +40,7 @@ cat $CENTER_IMAGE_LIST_FILE_PATH | while IFS=',' read -r part_sec fade_in_sec fa
 
     # 画像からフェード動画作成 背景は透明で、フェードイン、表示、フェードアウトする動画パーツ
     fade_out_start=$((part_sec - fade_out_sec))
-    ffmpeg -y -loop 1 -i $file_path -vf "scale=1422:800,format=rgba,fade=t=in:st=0:d=${fade_in_sec}:alpha=1,fade=t=out:st=${fade_out_start}:d=${fade_out_sec}:alpha=1" -t ${part_sec} -c:v qtrle $FADE_MOVIE_PATH < /dev/null
+    ffmpeg -y -loop 1 -i $file_path -vf "scale=${center_image_width}:${center_image_height},format=rgba,fade=t=in:st=0:d=${fade_in_sec}:alpha=1,fade=t=out:st=${fade_out_start}:d=${fade_out_sec}:alpha=1" -t ${part_sec} -c:v qtrle $FADE_MOVIE_PATH < /dev/null
     
     # ベース動画とフェード動画から動画パート作成
     ffmpeg -y -i $BASE_MOVIE_PATH -i $FADE_MOVIE_PATH -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,${part_sec})'" -c:a copy $movie_part_file_path < /dev/null
