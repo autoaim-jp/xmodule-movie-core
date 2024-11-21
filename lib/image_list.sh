9a027e377cc4a272dfa0cd0e8ba30607c827a1b6
autoaim_jp <git@autoaim.jp>
2024-11-22 00:45:39 +0900

    feat: update children_book_2.sh

--- lib/image_list.sh
+++ lib/image_list.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 IMAGE_DIR_PATH=${1:-./data/src/project/children_book_1/dalle/}
-find $(pwd)/${IMAGE_DIR_PATH} -type f | sort
+# 先頭のドットを取り除く
+IMAGE_DIR_PATH=${IMAGE_DIR_PATH#.}
+find $(pwd)${IMAGE_DIR_PATH} -type f | sort
 
