acf612930ae65cce28ebe24f6df842edd59738ba
autoaim_jp <git@autoaim.jp>
2024-11-22 02:29:06 +0900

    feat: image_list.csv from voicepeak.sh

--- children_book_2.sh
+++ children_book_2.sh
@@ -17,7 +17,7 @@ NARRATION_FILE_PATH=${2} # narration.csv
 TITLE_TEXT=${3} # 物語のタイトル
 TITLE_IMAGE_FILE_PATH=${4} # 物語の表紙画像
 TELOP_TEXT=${5} # 右上の画像
-IMAGE_LIST_FILE_PATH=${6} # 挿絵のパスリスト
+IMAGE_DIR_PATH=${6} # 挿絵画像のディレクトリ
 
 # tmp
 mkdir -p "$TMP_DIR_PATH"
@@ -28,9 +28,10 @@ TMP_RT_FILE_PATH="${TMP_DIR_PATH}__black_white_rt.png"
 TMP_MAIN_PART_MOVIE_FILE_PATH="${TMP_DIR_PATH}__main_part_movie.mp4"
 TMP_CONCAT_MOVIE_FILE_PATH="${TMP_DIR_PATH}__concat_movie.mp4"
 TMP_SUBTITLE_MOVIE_FILE_PATH="${TMP_DIR_PATH}__subtitle_movie.mp4"
+TMP_IMAGE_LIST_FILE_PATH="${TMP_DIR_PATH}__image_list.csv"
 
 # ./app/speak_sound.sh
-./app/speak_sound.sh "$TMP_SOUND_FILE_PATH" "$TMP_SUBTITLE_FILE_PATH" "$NARRATION_FILE_PATH"
+./app/speak_sound.sh "$TMP_SOUND_FILE_PATH" "$TMP_SUBTITLE_FILE_PATH" "$TMP_IMAGE_LIST_FILE_PATH" "$NARRATION_FILE_PATH" "$IMAGE_DIR_PATH"
 
 # ./app/title_movie.sh
 ./app/title_movie.sh "$TMP_TITLE_MOVIE_FILE_PATH" "$TITLE_TEXT" "$TITLE_IMAGE_FILE_PATH" 8
@@ -39,7 +40,7 @@ TMP_SUBTITLE_MOVIE_FILE_PATH="${TMP_DIR_PATH}__subtitle_movie.mp4"
 ./app/telop_image.sh - - "$TMP_RT_FILE_PATH" - - "$TELOP_TEXT" "$FONT_FILE_PATH"
 
 # ./app/main_part_from_image_list.sh
-./app/main_part_from_image_list.sh "$TMP_MAIN_PART_MOVIE_FILE_PATH" "$IMAGE_LIST_FILE_PATH" "$BACKGROUND_IMAGE_FILE_PATH" x "$TMP_RT_FILE_PATH" x x
+./app/main_part_from_image_list.sh "$TMP_MAIN_PART_MOVIE_FILE_PATH" "$TMP_IMAGE_LIST_FILE_PATH" "$BACKGROUND_IMAGE_FILE_PATH" x "$TMP_RT_FILE_PATH" x x
 
 # ./app/concat_movie.sh
 ./app/concat_movie.sh "$TMP_CONCAT_MOVIE_FILE_PATH" "$TMP_TITLE_MOVIE_FILE_PATH" "$TMP_MAIN_PART_MOVIE_FILE_PATH" "$TMP_SOUND_FILE_PATH"
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./children_book_2.sh ./data/result/children_book_3/result.mp4 ${PWD}/data/src/project/children_book_3/narration.csv "お菓子の魔法" ${PWD}/data/src/project/children_book_3/3_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_3/asset_ordered/ | tee log/20241122_0216.log
+
