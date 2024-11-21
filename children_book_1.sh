072a6c182c08557167f51e0cc6ce5c9247ce4d12
autoaim_jp <git@autoaim.jp>
2024-11-22 03:03:12 +0900

    feat: update start.sh asset/ to asset_ordered

--- children_book_1.sh
+++ children_book_1.sh
@@ -5,7 +5,7 @@ OUTPUT_MOVIE_FILE_PATH=${PWD}/data/result/children_book_1/result.mp4
 
 # ./app/capture.sh
 
-./app/speak_sound.sh "${TMP_DIR_PATH}__speak_sound.wav" "${TMP_DIR_PATH}__subtitle.csv" "${TMP_DIR_PATH}__image_list.scv" ./data/src/project/children_book_1/narration.csv ${PWD}/data/src/project/children_book_1/asset_ordered/
+./app/speak_sound.sh "${TMP_DIR_PATH}__speak_sound.wav" "${TMP_DIR_PATH}__subtitle.csv" "${TMP_DIR_PATH}__image_list.csv" ./data/src/project/children_book_1/narration.csv ${PWD}/data/src/project/children_book_1/asset_ordered/
 
 ./app/title_movie.sh "${TMP_DIR_PATH}__title_movie.mp4" "もりのぼうけんしゃ\nタケルとふしぎなタネ" ./data/src/project/children_book_1/dalle/title.webp 8
 
@@ -13,7 +13,7 @@ OUTPUT_MOVIE_FILE_PATH=${PWD}/data/result/children_book_1/result.mp4
 
 # ./app/main_part_from_capture.sh
 # ./app/main_part_from_image_list.sh "${TMP_DIR_PATH}__main_part_movie.mp4" ./data/src/project/children_book_1/image_list.txt ./data/src/project/children_book_1/bg_ccffcc.png x "${TMP_DIR_PATH}__black_white_title.png" x x
-./app/main_part_from_image_list.sh "${TMP_DIR_PATH}__main_part_movie.mp4" "${TMP_DIR_PATH}__image_list.scv" ./data/src/project/children_book_1/bg_ccffcc.png x "${TMP_DIR_PATH}__black_white_title.png" x x
+./app/main_part_from_image_list.sh "${TMP_DIR_PATH}__main_part_movie.mp4" "${TMP_DIR_PATH}__image_list.csv" ./data/src/project/children_book_1/bg_ccffcc.png x "${TMP_DIR_PATH}__black_white_title.png" x x
 
 ./app/concat_movie.sh "${TMP_DIR_PATH}__concat_movie.mp4" "${TMP_DIR_PATH}__title_movie.mp4" "${TMP_DIR_PATH}__main_part_movie.mp4" "${TMP_DIR_PATH}__speak_sound.wav"
 
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./children_book_2.sh ./data/result/children_book_3/result.mp4 ${PWD}/data/src/project/children_book_3/narration.csv "お菓子の魔法" ${PWD}/data/src/project/children_book_3/3_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_3/asset_ordered/ | tee log/20241122_0216.log
+# ./children_book_2.sh ./data/result/children_book_3/result.mp4 ${PWD}/data/src/project/children_book_3/narration.csv "お菓子の魔法" ${PWD}/data/src/project/children_book_3/3_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_3/asset/ | tee log/20241122_0250.log
 
+./children_book_2.sh ./data/result/children_book_4/result.mp4 ${PWD}/data/src/project/children_book_4/narration.csv "シマリス兄弟とふしぎなショッピングモール" ${PWD}/data/src/project/children_book_4/6_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_4/asset/ | tee log/20241122_0300.log
