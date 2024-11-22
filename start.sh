1fc911ebccfb1364db67510275f35f042c2a585a
autoaim_jp <git@autoaim.jp>
2024-11-23 00:35:17 +0900

    refactoring: start.sh, main.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# ./children_book_2.sh ./data/result/children_book_3/result.mp4 ${PWD}/data/src/project/children_book_3/narration.csv "お菓子の魔法" ${PWD}/data/src/project/children_book_3/3_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_3/asset/ | tee log/20241122_0250.log
+./main.sh ./data/result/children_book_1/result.mp4 ${PWD}/data/src/project/children_book_1/narration.csv "もりのぼうけんしゃ\nタケルとふしぎなタネ" ${PWD}/data/src/project/children_book_1/title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_1/dalle/ | tee log/20241123_children_book_1.log
 
-./children_book_2.sh ./data/result/children_book_4/result.mp4 ${PWD}/data/src/project/children_book_4/narration.csv "シマリス兄弟とふしぎなショッピングモール" ${PWD}/data/src/project/children_book_4/6_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_4/asset/ | tee log/20241122_0300.log
+# ./main.sh ./data/result/children_book_3/result.mp4 ${PWD}/data/src/project/children_book_3/narration.csv "お菓子の魔法" ${PWD}/data/src/project/children_book_3/3_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_3/asset/ | tee log/20241122_0250.log
+
+
+# ./main.sh ./data/result/children_book_4/result.mp4 ${PWD}/data/src/project/children_book_4/narration.csv "シマリス兄弟とふしぎなショッピングモール" ${PWD}/data/src/project/children_book_4/6_title.webp "Tempra\nTitans" ${PWD}/data/src/project/children_book_4/asset/ | tee log/20241122_0300.log
