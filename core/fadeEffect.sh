2f6ca453cc92597934846afa1c7d5a623140a951
autoaim_jp <git@autoaim.jp>
2025-02-06 04:18:57 +0900

    feat: merge title effect

--- core/fadeEffect.sh
+++ core/fadeEffect.sh
@@ -12,11 +12,16 @@ TITLE_IMG_FILE_PATH=${2:-"${SAMPLE_PROJECT_DIR_PATH}title.png"}
 TITLE_SEC=${3:-5}  # デフォルトは5秒
 
 # tmp
-TMP_FADEIN_FILE_PATH="${TMP_DIR_PATH}__fadein.mp4"
+# TMP_FADEIN_FILE_PATH="${TMP_DIR_PATH}__fadein.mp4"
 
 # other
 
-ffmpeg -y -loop 1 -framerate 60 -i $TITLE_IMG_FILE_PATH -vf "fade=in:0:30,format=yuv420p" -t ${TITLE_SEC} -c:v h264_nvenc -b:v 4M -maxrate 6M -bufsize 8M -preset slow -profile:v high -rc-lookahead 32 -preset fast $TMP_FADEIN_FILE_PATH
-     
-ffmpeg -y -i $TMP_FADEIN_FILE_PATH -vf "fade=out:120:30,format=yuv420p" -c:v h264_nvenc -b:v 4M -maxrate 6M -bufsize 8M -preset slow -profile:v high -rc-lookahead 32 -preset fast $TITLE_MOVIE_FILE_PATH
+# 一つに統合できる
+# ffmpeg -y -loop 1 -framerate 60 -i $TITLE_IMG_FILE_PATH -vf "fade=in:0:30,format=yuv420p" -t ${TITLE_SEC} -c:v h264_nvenc -b:v 4M -maxrate 6M -bufsize 8M -preset slow -profile:v high -rc-lookahead 32 -preset fast $TMP_FADEIN_FILE_PATH
+# ffmpeg -y -i $TMP_FADEIN_FILE_PATH -vf "fade=out:120:30,format=yuv420p" -c:v h264_nvenc -b:v 4M -maxrate 6M -bufsize 8M -preset slow -profile:v high -rc-lookahead 32 -preset fast $TITLE_MOVIE_FILE_PATH
+
+# 統合
+ffmpeg -y -loop 1 -framerate 60 -i "$TITLE_IMG_FILE_PATH" \
+-vf "fade=in:0:30,fade=out:120:30,format=yuv420p" -t "${TITLE_SEC}" \
+-c:v h264_nvenc -b:v 4M -maxrate 6M -bufsize 8M -preset slow -profile:v high -rc-lookahead 32 -preset fast "$TITLE_MOVIE_FILE_PATH"
 
